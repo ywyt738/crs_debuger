fb81f3c812362ad5f3c63f4c8f3debc40146b6be
HuangXiaojun <120968816@qq.com>
2019-01-16 11:24:52 +0800

    补充ci

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="crs_debuger",
-    version="0.0.2",
+    version="0.0.3",
     author="HuangXiaojun",
     author_email="huangxiaojun@sightp.com",
     description="crs debuger",
